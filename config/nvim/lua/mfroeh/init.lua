d31052b217e411ba967eee54add5545b78a089ad
mfroeh <mfroeh0@pm.me>
2023-01-06 15:36:11 +0100

    Add some useful nixos/home-manager vim functions

--- config/nvim/lua/mfroeh/init.lua
+++ config/nvim/lua/mfroeh/init.lua
@@ -3,3 +3,22 @@ require("mfroeh.set")
 require("mfroeh.misc")
 
 vim.cmd('colorscheme rose-pine')
+
+
+FLAKE_PATH = "$HOME/dotnix";
+
+HOME_MANAGER_SWITCH = function()
+  vim.fn.termopen("home-manager switch --flake " .. FLAKE_PATH .. "#$USER" .. "@" .. "$hostname")
+   -- TODO: This does not show output yet
+  -- vim.fn.jobstart("home-manager switch --flake " .. FLAKE_PATH .. "#$USER" .. "@" .. "$hostname", { pty = true, })
+end
+
+NIXOS_REBUILD_SWITCH = function()
+  vim.fn.termopen("sudo nixos-rebuild switch --flake " .. FLAKE_PATH .. "#$hostname")
+end
+
+vim.api.nvim_create_user_command("HomeManagerSwitch", HOME_MANAGER_SWITCH, {})
+vim.api.nvim_create_user_command("NixosRebuildSwitch", NIXOS_REBUILD_SWITCH, {})
+
+vim.keymap.set("n", "<leader>nn", NIXOS_REBUILD_SWITCH)
+vim.keymap.set("n", "<leader>hh", HOME_MANAGER_SWITCH)
