79b54db6644ba8f919c977cb046df5304aae4256
mfroeh <mfroeh0@pm.me>
2025-07-03 15:34:31 +0200

    remove some unused substituters

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,27 +1,31 @@
-{ pkgs, lib, ... }: {
+{ pkgs, lib, ... }:
+{
   nix = {
     settings = {
       # enable new nix cli and flakes
-      experimental-features = [ "nix-command" "flakes" ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
       # enable community caches
       substituters = [
         "https://nix-community.cachix.org"
-        "https://hyprland.cachix.org"
-        "https://cuda-maintainers.cachix.org"
-        "https://ai.cachix.org"
-        "https://ploop.cachix.org" # jaxlib
         "https://devenv.cachix.org"
-        "https://walker-git.cachix.org"
+        # "https://hyprland.cachix.org"
+        # "https://cuda-maintainers.cachix.org"
+        # "https://ai.cachix.org"
+        # "https://ploop.cachix.org" # jaxlib
+        # "https://walker-git.cachix.org"
       ];
       # public keys for the caches
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-        "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
-        "ploop.cachix.org-1:i6+Fqarsbf5swqH09RXOEDvxy7Wm7vbiIXu4A9HCg1g="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
+        # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        # "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        # "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
+        # "ploop.cachix.org-1:i6+Fqarsbf5swqH09RXOEDvxy7Wm7vbiIXu4A9HCg1g="
+        # "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
       ];
       trusted-users = [ "root mo" ];
       # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
@@ -31,12 +35,21 @@
     # automatically run the nix store optimiser at a specific time
     optimise.automatic = true;
 
-    gc = lib.mkIf pkgs.stdenv.isLinux {
-      automatic = true;
-    } // lib.optionalAttrs pkgs.stdenv.isLinux {
-      dates = "weekly";
-    } // lib.optionalAttrs pkgs.stdenv.isDarwin {
-      interval = [ { Hour = 0; Minute = 0; Weekday = 7; } ];
-    };
+    gc =
+      lib.mkIf pkgs.stdenv.isLinux {
+        automatic = true;
+      }
+      // lib.optionalAttrs pkgs.stdenv.isLinux {
+        dates = "weekly";
+      }
+      // lib.optionalAttrs pkgs.stdenv.isDarwin {
+        interval = [
+          {
+            Hour = 0;
+            Minute = 0;
+            Weekday = 7;
+          }
+        ];
+      };
   };
 }
