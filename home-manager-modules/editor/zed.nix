cb0ef9734beac2148f89cd6f280d7a503e2a8aa6
mfroeh <mfroeh0@pm.me>
2025-07-05 20:12:35 +0200

    set up linting for go - nice

--- home-manager-modules/editor/zed.nix
+++ home-manager-modules/editor/zed.nix
@@ -2,17 +2,18 @@
 {
   programs.zed-editor = {
     enable = true;
+    extraPackages = with pkgs; [
+      nixd
+      vscode-json-languageserver
+      rust-analyzer
+
+      golangci-lint
+      golangci-lint-langserver
+    ];
   };
 
   xdg.configFile."zed/settings.json".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotnix/config/zed/settings.json";
   xdg.configFile."zed/keymap.json".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotnix/config/zed/keymap.json";
-
-  # lsps, formatters, etc.
-  home.packages = with pkgs; [
-    # nix
-    nixd
-    nixfmt-rfc-style
-  ];
 }
