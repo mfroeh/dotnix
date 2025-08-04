1191dc2ffb41d538be867745c639e260d64aed04
mfroeh <mfroeh0@pm.me>
2025-08-04 22:59:10 +0200

    nix flake update and zed

--- home-manager-modules/editor/zed.nix
+++ home-manager-modules/editor/zed.nix
@@ -10,11 +10,14 @@
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotnix/config/zed/keymap.json";
 
   home.packages = with pkgs; [
+    # nix
     nixd
     nixfmt-rfc-style
 
+    # json
     vscode-json-languageserver
 
+    # rust
     (rust-bin.selectLatestNightlyWith (
       toolchain:
       toolchain.default.override {
@@ -22,10 +25,12 @@
       }
     ))
 
+    # go
     gopls
     golangci-lint
     golangci-lint-langserver
 
+    # c++
     clang-tools
 
     # toml
